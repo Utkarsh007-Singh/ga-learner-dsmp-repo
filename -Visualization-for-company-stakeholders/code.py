db1aadd36a2d22cbfbc70509398f3073e04fe1e7
Utkarsh007-Singh <Utkarsh007-Singh@users.noreply.github.com>
2019-09-05 16:16:50 +0530

    Code file created.

---
+++ -Visualization-for-company-stakeholders/code.py
@@ -0,0 +1,80 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#Loading the data
+data = pd.read_csv(path)
+loan_status = data['Loan_Status'].value_counts()
+loan_status.plot(kind='bar')
+plt.title('visualizing the companys record with respect to loan approvals')
+plt.xlabel('Loan Status')
+plt.ylabel('VALUES')
+plt.show()
+
+
+
+
+#Code starts here
+
+
+# --------------
+#Code starts here
+property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
+print(property_and_loan)
+property_and_loan.plot(kind='bar',stacked=False)
+plt.xlabel('Property Area')
+plt.ylabel('Loan status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+
+# --------------
+#Code starts here
+education_and_loan = data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar',stacked = True)
+plt.xlabel('Education Status')
+plt.ylabel('Loan Status')
+plt.title('Expensive Education')
+plt.xticks(rotation=45)
+
+
+
+# --------------
+#Code starts here
+graduate=data[data['Education']=='Graduate']
+not_graduate=data[data['Education']=='Not Graduate']
+graduate['LoanAmount'].plot(kind='density',label='Graduate')
+not_graduate['LoanAmount'].plot(kind='density',label='Not Graduate')
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig ,(ax_1,ax_2,ax_3) = plt.subplots(nrows=3, ncols=1,figsize=(20,20))
+ax_1.scatter(data['ApplicantIncome'],data['LoanAmount'])
+ax_1.set(title='Applicant Income')
+ax_2.scatter(data['CoapplicantIncome'],data['LoanAmount'])
+ax_2.set(title='Coapplicant Income')
+data['TotalIncome'] = data['ApplicantIncome'] + data['CoapplicantIncome']
+ax_3.scatter(data['TotalIncome'],data['LoanAmount'])
+ax_3.set(title='Total Income')
+
+
